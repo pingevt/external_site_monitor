cbf65ede6e3d74bb914800d7833cfa09a87056cf
Pete Inge <pinge@bluecadet.com>
2023-03-19 14:35:11 -0400

    Add in some basic tests

---
+++ tests/src/Functional/BlcFuncTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Drupal\Tests\external_site_monitor\Functional;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ *
+ *
+ * @group amon_carter
+ */
+class BlcFuncTest extends BrowserTestBase {
+
+  /**
+   * The modules to load to run the test.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'node',
+    'field',
+    'text',
+    'options',
+    'external_site_monitor',
+  ];
+
+  /**
+   * Default theme.
+   *
+   * @var string
+   */
+  protected $defaultTheme = 'claro';
+
+  /**
+   * A user with administration rights.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $adminUser;
+
+  /**
+   * An authenticated user.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $authenticatedUser;
+
+  /**
+   * A test menu.
+   *
+   * @var \Drupal\system\Entity\Menu
+   */
+  protected $menu;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() : void {
+    parent::setUp();
+
+    $this->adminUser = $this->drupalCreateUser([
+      'access administration pages',
+    ]);
+    $this->authenticatedUser = $this->drupalCreateUser([]);
+
+  }
+
+  /**
+   * Test Basic Functionality.
+   */
+  public function testBasicFunc() {
+    $session = $this->assertSession();
+
+    $this->assertTrue(TRUE);
+  }
+
+}
---
+++ tests/src/Unit/BlcTests.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\Tests\external_site_monitor\Unit;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\esm_test_blc\SiteCrawlerController;
+
+/**
+ *
+ */
+class BlcTests extends UnitTestCase {
+
+  protected $siteCrawler;
+
+  /**
+   * Before a test method is run, setUp() is invoked.
+   * Create new unit object.
+   */
+  public function setUp() {
+    $this->siteCrawler = new SiteCrawlerController();
+  }
+
+  public function testCheckStaticVars() {
+
+    // Class Constants.
+  // const NEEDS_ROOT = -50;
+  // const NEEDS_SITEMAP = -10;
+  // const PROCCESSING_SITEMAP = -8;
+  // const WORKING = -2;
+  // const FINISHED = 1;
+
+  // const LABELS = [
+  //   self::NEEDS_ROOT => "Needs Root",
+  //   self::NEEDS_SITEMAP => "Needs Sitemap",
+  //   self::PROCCESSING_SITEMAP => "Processing Sitemap",
+  //   self::WORKING => "Working...",
+  //   self::FINISHED => "Finished",
+  // ];
+
+
+
+    $this->assertEquals(-50, SiteCrawlerController::NEEDS_ROOT);
+    $this->assertEquals(-10, SiteCrawlerController::NEEDS_SITEMAP);
+    $this->assertEquals(-8, SiteCrawlerController::PROCCESSING_SITEMAP);
+    $this->assertEquals(-2, SiteCrawlerController::WORKING);
+    $this->assertEquals(1, SiteCrawlerController::FINISHED);
+  }
+
+  /**
+   * Once test method has finished running, whether it succeeded or failed, tearDown() will be invoked.
+   * Unset the  object.
+   */
+  public function tearDown() {
+    unset($this->siteCrawler);
+  }
+
+}
